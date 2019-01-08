7d78a073c6e82a0578e5388c242d6da3ea7796cb
Yanwu.Ren <349426266@qq.com>
2019-01-08 01:05:54 -0700

    website

---
+++ TrumpSite/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/renyanwu/Documents/GitHub/crawler4Trump/TrumpSite/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
