e2466b3d98bed4b05fffe4775f9892bf43f96f59
Yanwu.Ren <349426266@qq.com>
2019-01-08 14:25:24 -0700

    some changes

--- crawler_twitter.py
+++ crawler_twitter.py
@@ -1,9 +1,9 @@
 import tweepy
  
-consumer_key = '76xWHfx9daJckY6VTzrOSaC5d'
-consumer_secret = 'irLoKyPrGRC71KFQlFphDezVGOAG410UlteIGMP9sVc1tF8I45'
-access_token = '1039739675949318147-PzHBrE3RWbAJpWBLPGGsvXi4f7Dv4I'
-access_token_secret = 'Pk6YlilJiHWVFepnLKaeDJipJHpcVjaBUeLqiuGnZIFOQ'
+consumer_key = 'xxxx'
+consumer_secret = 'xxxx'
+access_token = 'xxxx-xxxx'
+access_token_secret = 'xxxx'
  
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
