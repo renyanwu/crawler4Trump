a1f24fd482d445a4ade976dba5932514dd0a754c
Yanwu.Ren <349426266@qq.com>
2019-01-07 21:34:24 -0700

    Update crawer.py

---
+++ crawer.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def getpaths(paths, url):
+	headers= {
+		'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+	}
+	resp = requests.get(url, headers = headers)
+	resp.encoding='uft-8'
+	soup=BeautifulSoup(resp.text,'html.parser')
+
+	articles = soup.find_all("article", class_="cd")
+
+	for i in range(len(articles)-20, len(articles)):
+		path = articles[i].find_all("a")[0].attrs['href']
+		if path[1]!='v':
+			if path not in paths:
+				paths.append(path)
+	
+	return paths
+	
+def getContent(path):
+	url = "https://www.cnn.com"+path
+	print(url)
+	resp = requests.get(url)
+	resp.encoding='uft-8'
+	soup=BeautifulSoup(resp.text,'html.parser')
+	
+	title = soup.title.string
+	print(title)
+	
+	time = soup.find_all("p", class_="update-time")[0].get_text()
+	print(time)
+	
+	paragraphs = soup.find_all(class_="zn-body__paragraph")
+	p2str = []
+	
+	for p in paragraphs:
+		p2str.append(str(p))
+
+	
+	article = {
+		"title" : title,
+		"time" : time,
+		"paragraphs" : p2str
+	}
+	
+	return article
+
+		
+
+paths = getpaths([], "https://www.cnn.com/specials/politics/president-donald-trump-45")
+paths = getpaths(paths, "https://www.cnn.com/specials/politics/trump-white-house")
+
+articles = []
+#getContent(paths[0])
+
+for path in paths:
+	articles.append(getContent(path))
+
+
+with open("CNN.json","w") as f:
+	json.dump(articles,f)
+	print("Success...")
+
+
+	
+
